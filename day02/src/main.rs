aa100d72a9f2b51c403034133309ca8b6dc41e24
hvogeler <heiko.vogeler@hvo.de>
2024-12-03 11:03:35 +0000

    day02 part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,9 +1,77 @@
-use std::path::Path;
+use std::{default, path::Path};
 
 use common::{read_test_data, Error};
 
 fn main()  -> Result<(), Error> {
-    let example_data = read_test_data(Path::new("./day02/example.dat"))?;
-    println!("Example data: {}", example_data);
+    let data = read_test_data(Path::new("./day02/testdata.dat"))?;
+    println!("Example data: {}", data);
+    let mut safe_count = 0;
+    for line in data.lines() {
+        if Report::from(line).is_safe() {
+            safe_count += 1;
+        }
+    }
+    println!("Safe reports: {}", safe_count);
     Ok(())
+}
+
+#[derive(Debug, Default)]
+pub struct Report {
+    levels: Vec<i64>,
+    direction: Direction,
+}
+
+impl From<&str> for Report {
+    fn from(levels_str: &str) -> Self {
+        let levels: Vec<i64> = levels_str.split(" ").map(|s| s.parse::<i64>().unwrap()).collect();
+        let direction = Direction::from(&Pair(levels[0], levels[1]));
+
+        Self {
+            levels,
+            direction,
+        }
+    }
+}
+
+
+impl Report {
+    pub fn is_safe(&self) -> bool {
+        for i in 0..self.levels.len() - 1 {
+            let pair = Pair(self.levels[i], self.levels[i+1]);
+            let direction = Direction::from(&pair);
+            if direction != self.direction || pair.diff() > 3 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+#[derive(Debug)]
+pub struct Pair(i64, i64);
+
+impl Pair {
+    pub fn diff(&self) -> i64 {
+        (self.0 - self.1).abs()
+    }
+}
+
+#[derive(Debug, Default, PartialEq)]
+enum Direction {
+    Increasing,
+    Decreasing,
+    #[default]
+    NotStrictlyMonotonic,
+}
+
+impl From<&Pair> for Direction {
+    fn from(pair: &Pair) -> Self {
+        if pair.0 < pair.1 {
+            return Direction::Increasing;
+        }
+        if pair.0 > pair.1 {
+            return Direction::Decreasing;
+        }
+        Direction::NotStrictlyMonotonic
+    }
 }
