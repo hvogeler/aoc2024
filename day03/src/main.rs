14a54c3c877f69895a1970053f0af1b46330a92a
Heiko Vogeler <hvogeler@abinitio.com>
2024-12-04 20:59:18 +0100

    day03 start

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,19 +1,52 @@
-use std::{path::Path, str::FromStr};
+use std::{default, path::Path, str::FromStr};
 
 use common::{read_test_data, Error};
 use regex::Regex;
 
 fn main() -> Result<(), Error> {
-    let data = read_test_data(Path::new("./day03/testdata.dat"))?;
-    println!("Example data: {}", data);
+    let data = read_test_data(Path::new("./day03/example2.dat"))?;
+    // println!("Example data: {}", data);
     let muls = parse(&data);
     let mut sum = 0;
     for mul in muls {
-        println!("{} = {}", mul.token, mul.eval());
+        // println!("{} = {}", mul.token, mul.eval());
         sum += mul.eval();
     }
     println!("Sum of products: {}", sum);
+
+    // part 2
+
+    let mut state = ParseState::default();
+    let mut enabled_string: String = String::new();
+    let mut buf: Vec<char> = Vec::new();
+    let chars: Vec<char> = data.chars().collect();
+    let mut cursor = 0;
+    while cursor < chars.len() {
+        if cursor<chars.len() - 4 && chars[cursor..cursor+4] == ['d', 'o', '(', ')'] {
+            println!("do() at {}", cursor);
+            state = ParseState::Enabled;
+            cursor += 4;
+        }
+        if cursor<chars.len() - 7 && chars[cursor..cursor+7] == ['d', 'o', 'n', '\'', 't', '(', ')'] {
+            println!("don't()  at {}", cursor);
+            state = ParseState::Disabled;
+            cursor += 7;
+        }
+        if state == ParseState::Enabled {
+            buf.push(chars[cursor]);
+        }
+        cursor += 1;
+    }
+    enabled_string = buf.into_iter().collect();
+    println!("enabled_string: {}", enabled_string);
     Ok(())
+}
+
+#[derive(Debug, Default, PartialEq)]
+enum ParseState {
+    #[default]
+    Enabled,
+    Disabled,
 }
 
 #[derive(Debug)]
